afe9da05181b57e2cde2ccb878dcaa1184ca3f2f
herodang <bestervn@gmail.com>
2022-04-04 09:59:36 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-Anglart-Coure',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
